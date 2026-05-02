f433b6313f3ee47f203fc4f29befdcead698ae5c
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2026-05-02 20:16:56 -0300

    feat: update antigravity version and add claude-code to system packages

--- nixos/modules/dev-dependencies.nix
+++ nixos/modules/dev-dependencies.nix
@@ -7,12 +7,15 @@ let
   };
   latest_antigravity = unstable.antigravity.overrideAttrs (oldAttrs: rec {
     src = unstable.fetchurl {
-      # url = "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/1.20.6-5891862175809536/linux-x64/Antigravity.tar.gz";
-      # sha256 = "sha256-rTgr8yGmIW0H+Vrx9hPgP1oH/fb8ZjK3ac6D2Br91Wc=";
       url = "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/1.23.2-4781536860569600/linux-x64/Antigravity.tar.gz";
       sha256 = "sha256-UjKkBI/0+hVoXZqYG6T7pXPil/PvybdvY455S693VyU=";
     };
   });
+  claude-code-src = builtins.fetchTarball {
+    url = "https://github.com/sadjow/claude-code-nix/archive/8bd0a84bcfbd7e76eaa1c3421fc59861eb8a8f24.tar.gz";
+    sha256 = "sha256:10svmm63z178fjrkmmh7n1ahxqf9znn6bibk1zdqv8vrzkyrj3vi";
+  };
+  latest_claude-code = pkgs.callPackage "${claude-code-src}/package.nix" { };
 in
 {
   environment.systemPackages =
@@ -47,6 +50,6 @@ in
     ++ [
       #Yep, this day has come
       latest_antigravity
-
+      latest_claude-code
     ];
 }
