5ef5670cc2ca6c74751affdffb60ea55b4737820
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2026-05-21 20:07:41 -0300

    fix: hyprland

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "bak";
             home-manager.users.lalvesl = import ./nixos/modules/home/mod.nix;
           }
         ];
--- nixos/modules/home/wms/hyprland.nix
+++ nixos/modules/home/wms/hyprland.nix
@@ -3,7 +3,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
-    configType = "lua";
+    configType = "hyprlang";
 
     settings = {
       "$mainMod" = "SUPER";
