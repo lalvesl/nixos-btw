6cdf2cc48aa5b5674066ce82a0df99ebdc859e99
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2026-06-16 15:09:11 -0300

    feat: customize zsh agnoster prompt with path compression, hidden context, and execution duration display

--- nixos/modules/home/zsh.nix
+++ nixos/modules/home/zsh.nix
@@ -21,5 +21,39 @@
       ];
       theme = "agnoster";
     };
+
+    initContent = ''
+      _cmd_start=0
+      _cmd_duration_str=""
+
+      preexec() {
+        _cmd_start=$SECONDS
+      }
+
+      precmd() {
+        if (( _cmd_start > 0 )); then
+          local dur=$(( SECONDS - _cmd_start ))
+          local h=$(( dur / 3600 ))
+          local m=$(( (dur % 3600) / 60 ))
+          local s=$(( dur % 60 ))
+          if (( dur < 60 )); then
+            _cmd_duration_str="''${dur}s"
+          elif (( dur < 3600 )); then
+            _cmd_duration_str="$(printf '%dm%02ds' $m $s)"
+          else
+            _cmd_duration_str="$(printf '%dh%02dm%02ds' $h $m $s)"
+          fi
+        else
+          _cmd_duration_str=""
+        fi
+        _cmd_start=0
+
+        if [[ -n "$_cmd_duration_str" ]]; then
+          RPROMPT="%F{208}⏱ %B$_cmd_duration_str%b%f %F{240}│%f %F{039}%B%*%b%f"
+        else
+          RPROMPT="%F{039}%B%*%b%f"
+        fi
+      }
+    '';
   };
 }
